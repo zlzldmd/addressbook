5157253900389425946c9b06d6fbaedbe4986fa7
root <root@localhost.localdomain>
2019-07-18 14:20:49 +0900

    upsert

--- addressbook.cpp
+++ addressbook.cpp
@@ -1,12 +1,37 @@
-#include <eosiolib/eosio.hpp>
+#include <eosio/eosio.hpp>
 
 using namespace eosio;
 
-CONTRACT addressbook: public contract {
+CONTRACT addressbook: public contract{
     public:
-    using contract::contract;
-        ACTION upsert() {}
-        ACTION insert(name user, std::string first_name, std::string last_name, uint32_t age) {
+        using contract::contract;
+     
+        
+        ACTION upsert(name user, std::string first_name, std::string last_name, uint32_t age) {
+            require_auth(user);
+           
+            address_index forUpsert(get_self(), get_self().value);
+            auto itr = forUpsert.find(user.value); //auto 뒤에 오는 변수의 자료형을 알아서 바꿔주어 자료형을 안써줘도됨
+            
+            if(itr == forUpsert.end() )
+            {
+            forUpsert.emplace(user, [&](auto& row){
+                row.user = user;
+                row.first_name = first_name;
+                row.last_name = last_name;
+                row.age = age;
+            });}
+            else { 
+                forUpsert.modify(itr, user, [&](auto& row){
+                    row.user = user;
+                    row.first_name = first_name;
+                    row.last_name = last_name;
+                    row.age = age;
+                });
+            }
+            print("upsert success");
+        }
+         ACTION insert(name user, std::string first_name, std::string last_name, uint32_t age) {
             require_auth(user);
             address_index forInsert(get_self(), get_self().value);
             auto itr = forInsert.find(user.value);
@@ -18,6 +43,7 @@ CONTRACT addressbook: public contract {
                 row.age = age;
             print("insert success");
             });
+        
         }
         ACTION erase(name user) {
             require_auth(user);
